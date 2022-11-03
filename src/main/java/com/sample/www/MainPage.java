75d57b47ee94663e82da613b93ba213cdac3fe7b
galihanggara68 <galihanggara68@gmail.com>
2022-11-03 11:23:23 +0700

    Fixing

--- src/main/java/com/sample/www/MainPage.java
+++ src/main/java/com/sample/www/MainPage.java
@@ -4,9 +4,9 @@ import org.openqa.selenium.By;
 
 public class MainPage {
 
-    public static By btnSearch = By.id("gi_search_btn");
-    public static By txtSource = By.id("gosuggest_inputSrc");
-    public static By txtDestination = By.id("gosuggest_inputDest");
-    public static By txtDepartureCalendar = By.id("departureCalendar");
+    public static By btnSearch = By.xpath("//*[@id='root']/div[4]/div/div/div[3]/span");
+    public static By txtSource = By.xpath("//*[@id='root']/div[3]/div/div/div[1]/div[1]/div/div[2]/div/input");
+    public static By txtDestination = By.xpath("//*[@id='root']/div[3]/div/div/div[1]/div[2]/div/div[2]/div/input");
+    public static By txtDepartureCalendar = By.xpath("//*[@id='root']/div[3]/div/div/div[1]/div[3]/div/p[1]");
     public static By selectDate = By.xpath("(//div[@class='DayPicker-Day'])[18]");
 }
